ac13024d986e928096d5ce5cf9f733e0bf31fe36
Serious Goose <seriousgoosedev@gmail.com>
2024-10-14 22:16:07 -0400

    play -> opens game scene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,13 +2,13 @@ extends Control
 
 func _on_play_pressed() -> void:
 	print('Play Game!')
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 	pass # Replace with function body.
 
 
 func _on_options_pressed() -> void:
 	print('View Options Menu')
 	get_tree().change_scene_to_file("res://scenes/menus/options.tscn")
-	pass # Replace with function body.
 
 
 func _on_exit_pressed() -> void:
