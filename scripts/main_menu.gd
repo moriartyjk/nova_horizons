deb95c7e07df5d5e10526b0f35afebb4b63f723c
Serious Goose <seriousgoosedev@gmail.com>
2024-10-14 21:53:40 -0400

    each button displays appropriate debug message

---
+++ scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_play_pressed() -> void:
+	print('Play Game!')
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	print('View Options Scene')
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	print('Goodbye')
+	#get_tree().quit()
+	pass # Replace with function body.
