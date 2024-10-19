a9531a9b9a2fc321597d37a161fadeae79f122b2
Serious Goose <seriousgoosedev@gmail.com>
2024-10-19 17:15:57 -0400

    on timeout, reload current scene == which is the game scene

--- scripts/countdown.gd
+++ scripts/countdown.gd
@@ -2,7 +2,6 @@ extends Label
 
 @onready var countdown: Timer = $Countdown
 
-
 func _process(_delta : float) -> void:
 	text = _format_seconds(countdown.time_left, false)
 	
@@ -16,3 +15,8 @@ func _format_seconds(time : float, use_milliseconds : bool) -> String:
 	var milliseconds := fmod(time, 1) * 100
 
 	return "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
+
+
+func _on_countdown_timeout() -> void:
+	get_tree().reload_current_scene()
+	#pass # Replace with function body.
