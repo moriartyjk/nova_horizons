40c68c917a25b1b0628dcc78b1ff7a4cb375d8d2
Serious Goose <seriousgoosedev@gmail.com>
2024-10-19 22:11:28 -0400

    created Interactable class to divy out the types of interactable objects in the game

--- interact/interact_ray.gd
+++ interact/interact_ray.gd
@@ -6,4 +6,7 @@ func _physics_process(delta: float) -> void:
 	prompt.text = ""
 	
 	if is_colliding():
-		prompt.text = "something else..."
+		var collider = get_collider()
+		
+		if collider is Interactable:
+			prompt.text = collider.prompt_message
