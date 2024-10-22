8391ef85a235243356857a0bd770f49325d7c4cb
Serious Goose <seriousgoosedev@gmail.com>
2024-10-21 22:56:00 -0400

    Added pause menu scene; press escape to show hide menu; ceases movement in game and uncaptures mouse

---
+++ scripts/game.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@onready var pause_menu: Control = $UI/PauseMenu
+var paused = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+	
+		
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	
+	paused = !paused
