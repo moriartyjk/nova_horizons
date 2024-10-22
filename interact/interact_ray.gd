521b40dce305a81e9c55423ea078f441f5acaf17
Serious Goose <seriousgoosedev@gmail.com>
2024-10-21 21:02:59 -0400

    got my test *button* to interact with the script properly

--- interact/interact_ray.gd
+++ interact/interact_ray.gd
@@ -15,6 +15,3 @@ func _physics_process(delta: float) -> void:
 			
 			if Input.is_action_just_pressed(collider.prompt_input):
 				collider.interact(owner)
-			
-		else:
-			prompt.text = "something's wrong"
