572c742b8f5346d085a0871ba1de2e3fc57925f4
Jennifer Moriarty <jkmoriarty8@gmail.com>
2024-10-27 16:38:41 -0400

    main menu update... completely unrelated to video settings

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -3,13 +3,14 @@ extends Control
 func _on_play_pressed() -> void:
 	print('Play Game!')
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_settings_pressed() -> void:
+	print('View Settings Menu')
+	get_tree().change_scene_to_file("res://scenes/menus/settings.tscn")
+
+func _on_controls_pressed() -> void:
+	print('View Controls Page')
 	pass # Replace with function body.
-
-
-func _on_options_pressed() -> void:
-	print('View Options Menu')
-	get_tree().change_scene_to_file("res://scenes/menus/options.tscn")
-
 
 func _on_exit_pressed() -> void:
 	print('Goodbye')
---
+++ scripts/menu/video_settings.gd
@@ -0,0 +1,17 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func toggle_fullscreen(width: int, height: int):
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
