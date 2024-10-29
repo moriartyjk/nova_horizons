6afa1177dce2b08c3fb7dd16e84dcc10a454b2b6
Jennifer Moriarty <jkmoriarty8@gmail.com>
2024-10-28 21:35:04 -0400

    add resolution options to video settings page

---
+++ scripts/user_interface/resolution_mode_btn.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var option_button: OptionButton = $HBoxContainer/OptionButton
+
+const RESOLUTION_DICT: Dictionary = {
+	"1152 x 648": Vector2i(1152, 648),
+	"1280 x 720": Vector2i(1280, 720),
+	"1920 x 1080": Vector2i(1920, 1080),
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_resolution_items()
+	#pass # Replace with function body.
+
+func add_resolution_items() -> void:
+	for resolution_text in RESOLUTION_DICT:
+		option_button.add_item(resolution_text)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	pass # Replace with function body.
