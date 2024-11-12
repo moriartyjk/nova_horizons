94ac6ba9a49b46fbcabdcf64ef9b83f87f4bb019
Jennifer Moriarty <jkmoriarty8@gmail.com>
2024-11-11 19:51:20 -0500

    removed unneccessary ready and process methods

--- scripts/menu/window_mode_btn.gd
+++ scripts/menu/window_mode_btn.gd
@@ -2,15 +2,6 @@ extends Control
 
 @onready var option_button: OptionButton = $HBoxContainer/OptionButton
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_option_button_item_selected(index: int) -> void:
 	match index:
 		0: #fullscreen
