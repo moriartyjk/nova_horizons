dd178ea812ade82655b2f74354a690678a85bc00
Jennifer Moriarty <jkmoriarty8@gmail.com>
2024-11-11 19:49:24 -0500

    happy with basic video settings

---
+++ scripts/menu/resolution_mode_btn.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var option_button: OptionButton = $HBoxContainer/OptionButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600, 900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+		3:
+			DisplayServer.window_set_size(Vector2i(1152, 648))
--- scripts/menu/window_mode_btn.gd
+++ scripts/menu/window_mode_btn.gd
@@ -2,27 +2,14 @@ extends Control
 
 @onready var option_button: OptionButton = $HBoxContainer/OptionButton
 
-
-const WINDOW_MODE_ARR : Array[String] = [
-	"Fullscreen",
-	"Windowed",
-	"Borderless Windowed",
-]
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_window_mode_items()
-	#_on_option_button_item_selected()
-	#pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func add_window_mode_items() -> void:
-	for mode in WINDOW_MODE_ARR:
-		option_button.add_item(mode)
 
 func _on_option_button_item_selected(index: int) -> void:
 	match index:
@@ -35,4 +22,3 @@ func _on_option_button_item_selected(index: int) -> void:
 		2: #borderless windowed
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
-	#pass # Replace with function body.
