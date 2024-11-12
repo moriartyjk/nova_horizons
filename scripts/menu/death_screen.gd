c5bd599c758df7387dba5480fd0c02d3025bbb37
Jennifer Moriarty <jkmoriarty8@gmail.com>
2024-11-11 21:23:03 -0500

    retry and quit successfully print to console

---
+++ scripts/menu/death_screen.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_retry_pressed() -> void:
+	print('Retry Mission')
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	print('Quit Game')
+	pass # Replace with function body.
