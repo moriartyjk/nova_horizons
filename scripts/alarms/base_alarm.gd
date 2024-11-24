78afc1e26102221d428b85f0b4c85fc8d55228af
Jennifer Moriarty <jkmoriarty8@gmail.com>
2024-11-23 20:34:56 -0500

    separated update_alarm to sound and silence alarm status

--- scripts/alarms/base_alarm.gd
+++ scripts/alarms/base_alarm.gd
@@ -15,13 +15,9 @@ func _on_alarm_status_updated(alarm_name: String, alarm_status: int):
 	pass
 
 func _on_interacted(body: Variant) -> void:
-	#pass # Replace with function body.
 	var alarm_status = AhsManager.get_current_status(alarm_name)
 	match alarm_status:
 		AhsManager.AlarmStatus.SILENT:
-			AhsManager.update_alarm_status(alarm_name, AhsManager.AlarmStatus.SOUNDING)
+			AhsManager.sound_alarm(alarm_name)
 		AhsManager.AlarmStatus.SOUNDING:
-			AhsManager.update_alarm_status(alarm_name, AhsManager.AlarmStatus.SILENT)
-			
-	
-	
+			AhsManager.silence_alarm(alarm_name)
