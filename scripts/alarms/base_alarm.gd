f18fe63d226810703090ad9145971e4a49fbde5a
Jennifer Moriarty <jkmoriarty8@gmail.com>
2024-11-25 19:47:01 -0500

    Siren sounds when alarm goes off, overlaps eachother, turns off when alarm is silenced

--- scripts/alarms/base_alarm.gd
+++ scripts/alarms/base_alarm.gd
@@ -1,6 +1,7 @@
 extends Interactable
 
 @export var alarm_name: String
+@onready var alarm_siren: AudioStreamPlayer3D = $AlarmSiren
 
 func _ready() -> void:
 	prompt_message = "Sound Alarm"
@@ -12,8 +13,10 @@ func _on_alarm_status_updated(updated_name: String, updated_status: int):
 	
 		if(updated_status == 1):
 			prompt_message = "Silence Alarm"
+			alarm_siren.play()
 		else:
 			prompt_message = "Sound Alarm"
+			alarm_siren.stop()
 
 func _on_interacted(body: Variant) -> void:
 	var alarm_status = AhsManager.get_current_status(alarm_name)
